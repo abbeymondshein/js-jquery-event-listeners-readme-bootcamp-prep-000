2bf94cb5ff291ca93826d9952fa6f11ce6fa30a3
Abbey Mondshein <abbey.sync@gmail.com>
2018-11-06 02:05:07 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt(){
 
 //Define a function pressIt that does not accept a parameter. The function should bind a keydown event to the input field of the form that alerts a user when they have pressed the G key.
 
-
+//Define a function submitIt that does not accept a parameter. The function should bind a submit event to the form that alerts "Your form is going to be submitted now.".
 
 
 $(document).ready(function(){
