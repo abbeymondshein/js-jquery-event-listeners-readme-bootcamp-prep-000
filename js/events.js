192ff7f4159ef2728b17cd32baf6257e7416f528
Abbey Mondshein <abbey.sync@gmail.com>
2018-11-06 02:05:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,6 @@
 //define functions here
+//Define a function getIt that does not accept a parameter. The function should bind a click event to the p tag. When the paragraph is clicked, the function should alert "Hey!".
+
 function getIt(){
   $('p').on('click', alert('Hey!'));
 }
